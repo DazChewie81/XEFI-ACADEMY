970b694056cde1d3d3ffae4f8ecfe5e823b92074
DazChewie <62639989+DazChewie81@users.noreply.github.com>
2022-10-18 17:55:21 +0200

    Update main.py

--- jeu_python/main.py
+++ jeu_python/main.py
@@ -8,45 +8,49 @@ import Monstre
 import Guerrier
 import Mage
 import Sauvegarde
+
+class start(tk.Tk): 
+    def __init__(self):
+        
+        super().__init__()
+        self.button = ttk.Button(self, text='Nouvelle partie')
+        self.button.pack()
+        self.__bind()
+    
+    def __bind(self):
+        self.button.bind("<Button-1>", self.newgame)
+    
+    def newgame(self, event=None):
+        simon = Personnage.Personnage("Pendaroue", 30)
+        print(simon.pseudo)
+        print(orc.pv)
+        simon.attaque(baton, orc)
+        print(orc.pv)
+        
  
 
+    #instantiation amre
+    baton = Arme.Arme("baton phénix", 5)
+    #instantiation de mage
+    mage = Mage.Mage(baton)
 
-#instantiation amre
-baton = Arme.Arme("baton phénix", 5)
-#instantiation de mage
-mage = Mage.Mage(baton)
+    #instantiation amre
+    epee = Arme.Arme("epee du débutant", 3)
+    #instantiation de guerrier
+    guerrier = Guerrier.Guerrier(epee)
 
-#instantiation amre
-epee = Arme.Arme("epee du débutant", 3)
-#instantiation de guerrier
-guerrier = Guerrier.Guerrier(epee)
+    guerrierMage = Classe.Classe([mage, guerrier])
 
-guerrierMage = Classe.Classe([mage, guerrier])
+    #création objet classe
+    testClasse = Classe.Classe(mage) 
+    orc = Monstre.Monstre("Orc", 15)
+    print(orc.nom)
 
-#création objet classe
-testClasse = Classe.Classe(mage) 
-orc = Monstre.Monstre("Orc", 15)
-print(orc.nom)
+    #simon = Personnage.Personnage("Pendaroue", 30)
+    #print(simon.pseudo)
+    #print(orc.pv)
+    #simon.attaque(baton, orc)
+    #print(orc.pv)
 
-#simon = Personnage.Personnage("Pendaroue", 30)
-#print(simon.pseudo)
-#print(orc.pv)
-#simon.attaque(baton, orc)
-#print(orc.pv)
-
-def __init__(self):
-        
-    super().__init__()
-    self.button = ttk.Button(self, text='Nouvelle partie')
-    self.__bind()
-    
-def __bind(self):
-    self.button.bind("<Button-1>", self.newgame)
-    
-def newgame(self, event=None):
-    simon = Personnage.Personnage("Pendaroue", 30)
-    print(simon.pseudo)
-    print(orc.pv)
-    simon.attaque(baton, orc)
-    print(orc.pv)
-        
+start=start()
+start.mainloop()
