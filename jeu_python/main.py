eabb665cd61f8c63133215457b5f526ba02ec095
DazChewie <62639989+DazChewie81@users.noreply.github.com>
2022-10-19 13:57:53 +0200

    ui

--- jeu_python/main.py
+++ jeu_python/main.py
@@ -8,27 +8,28 @@ import Monstre
 import Guerrier
 import Mage
 import Sauvegarde
+import ui
 
-class start(tk.Tk): 
-    def __init__(self):
+class Main(): 
+    #def __init__(self):
         
-        super().__init__()
-        self.button = ttk.Button(self, text='Nouvelle partie')
-        self.button.pack()
-        self.__bind()
+        #super().__init__()
+        #self.button = ttk.Button(self, text='Nouvelle partie')
+        #self.button.pack()
+        #self.__bind()
     
-    def __bind(self):
-        self.button.bind("<Button-1>", self.newgame)
+    #def __bind(self):
+        #self.button.bind("<Button-1>", self.newgame)
     
-    def newgame(self, event=None):
-        orc = Monstre.Monstre("Orc", 15 ,3)
-        baton = Arme.Arme("baton phénix", 5)
-        mage = Mage.Mage(baton)
-        simon = Personnage.Personnage("Pendaroue", 30, mage)
-        print(simon.pseudo)
-        print(f"Il reste {orc.pv} à l'orc")
-        simon.classe.attaque(baton, orc)
-        print(f"Il reste {orc.pv} à l'orc")
+    #def newgame(self, event=None):
+        #orc = Monstre.Monstre("Orc", 15 ,3)
+        #baton = Arme.Arme("baton phénix", 5)
+        #mage = Mage.Mage(baton)
+        #simon = Personnage.Personnage("Pendaroue", 30, mage)
+        #print(simon.pseudo)
+        #print(f"Il reste {orc.pv} à l'orc")
+        #simon.classe.attaque(baton, orc)
+        #print(f"Il reste {orc.pv} à l'orc")
         
  
 
@@ -36,7 +37,8 @@ class start(tk.Tk):
 
     save1.save()
 
-    
 
-start=start()
+ui=ui()
+ui.mainloop()
+main=start()
 start.mainloop()
