b88f8668be74eab680e4b0a34a89861bfa2f054c
pandaroue <simon.gressard@gmail.com>
2022-10-19 13:23:56 +0200

    ajout de la sauvegarde

--- jeu_python/main.py
+++ jeu_python/main.py
@@ -21,38 +21,22 @@ class start(tk.Tk):
         self.button.bind("<Button-1>", self.newgame)
     
     def newgame(self, event=None):
-        simon = Personnage.Personnage("Pendaroue", 30)
+        orc = Monstre.Monstre("Orc", 15 ,3)
+        baton = Arme.Arme("baton phénix", 5)
+        mage = Mage.Mage(baton)
+        simon = Personnage.Personnage("Pendaroue", 30, mage)
         print(simon.pseudo)
-        print(orc.pv)
-        simon.attaque(baton, orc)
-        print(orc.pv)
+        print(f"Il reste {orc.pv} à l'orc")
+        simon.classe.attaque(baton, orc)
+        print(f"Il reste {orc.pv} à l'orc")
         
  
 
     save1 = Sauvegarde.Sauvegarde("save1")
 
     save1.save()
-    #instantiation amre
-    baton = Arme.Arme("baton phénix", 5)
-    #instantiation de mage
-    mage = Mage.Mage(baton)
 
-     #arc = Arme.Arme("baton phénix", 2)
-     #instantiation amre
-     #epee = Arme.Arme("epee du débutant", 3)
-     #instantiation de guerrier
-     #guerrier = Guerrier.Guerrier(epee)
+    
 
-     #archer = Archer.Archer(arc)
-     orc = Monstre.Monstre("Orc", 15 ,3)
-     print(orc.nom)
-
-     simon = Personnage.Personnage("Pendaroue", 30, mage)
-     print(simon.pseudo)
-     print(orc.pv)
-     simon.classe.attaque(baton, orc)
-     print(orc.pv)
-     orc.attaque(simon)
-     print(simon.pv)
 start=start()
 start.mainloop()
